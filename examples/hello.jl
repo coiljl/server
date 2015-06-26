ae7fabec307b14ca6b73bc5cb5a2144cce5cf609
Jake Rosoman <jkroso@gmail.com>
2015-06-27 09:51:28 +1200

    update example

--- examples/hello.jl
+++ examples/hello.jl
@@ -1,19 +1,7 @@
 @require "Request" Request verb
 @require ".." start Response
 
-function handle(::Request{:GET})
-  Response(200, [
-    "Content-Type"=>"text/plain",
-    "Content-Length"=>"11"
-  ], "Hello World")
-end
+handle(r::Request{:GET}) = Response("Hello World")
+handle(r::Request) = Response("That was a $(verb(r)) request")
 
-function handle(r::Request)
-  msg = "That was a $(verb(r)) request"
-  Response(200, [
-    "Content-Type"=>"text/plain",
-    "Content-Length"=>string(sizeof(msg))
-  ], msg)
-end
-
-start(handle, 8000)
+start(handle, 8000) |> wait
