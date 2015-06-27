74d4a21de4bedbad587a485b4e45488bb0894b33
Jake Rosoman <jkroso@gmail.com>
2015-06-27 13:50:55 +1200

    inline handler

--- index.jl
+++ index.jl
@@ -9,24 +9,11 @@ if !method_exists(Base.start, (Base.TcpServer,))
 end
 
 ##
-# Listen for HTTP requests on `port`. When one arrives it
-# handles it asynchronously in a separate task. However it will
-# immediately return to blocking while waiting for the next
-# request making this an infinitely blocking function. So be
-# aware that any code that comes after it will never run
+# Listen for HTTP requests on `port`
 #
 function start(app::Function, port::Integer)
-  for connection in listen(port)
-    handle(connection, app)
+  for sock in listen(port)
+    write(sock, app(Request(sock))::Response)
+    close(sock)
   end
-end
-
-##
-# Passes incoming HTTP Requests to `app` and writes the
-# return value back to the client before closing the
-# connection
-#
-function handle(sock::Base.Socket, app::Function)
-  write(sock, app(Request(sock))::Response)
-  close(sock)
 end
