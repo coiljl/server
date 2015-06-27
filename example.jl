80af8756504fd8e4d5947d83dcffd264f4a6a86f
Jake Rosoman <jkroso@gmail.com>
2015-06-27 13:37:14 +1200

    don't bother with the task
    no point in using tasks at this level until it can be made to work
    accross threads

--- example.jl
+++ example.jl
@@ -1,7 +1,7 @@
 @require "Request" Request verb
-@require ".." start Response
+@require "." start Response
 
 handle(r::Request{:GET}) = Response("Hello World")
 handle(r::Request) = Response("That was a $(verb(r)) request")
 
-start(handle, 8000) |> wait
+start(handle, 8000)
--- index.jl
+++ index.jl
@@ -1,6 +1,13 @@
 @require "Response" Response
 @require "Request" Request
 
+# Add iteration protocol
+if !method_exists(Base.start, (Base.TcpServer,))
+  Base.start(s::Base.TcpServer) = s
+  Base.next(s::Base.TcpServer, _) = accept(s), s
+  Base.done(s::Base.TcpServer, _) = !isopen(s)
+end
+
 ##
 # Listen for HTTP requests on `port`. When one arrives it
 # handles it asynchronously in a separate task. However it will
@@ -9,9 +16,8 @@
 # aware that any code that comes after it will never run
 #
 function start(app::Function, port::Integer)
-  server = listen(port)
-  @schedule while true
-    handle(accept(server), app)
+  for connection in listen(port)
+    handle(connection, app)
   end
 end
 
