af91271d9ef80770e13e9930dd510eac09ae4954
Jake Rosoman <jkroso@gmail.com>
2018-09-18 23:03:40 +1000

    update binary

--- examples/binary.jl
+++ examples/binary.jl
@@ -1,3 +1,3 @@
 handle(r::Request{:GET}) = Response("Hello World")
 handle(r::Request{:PUT}) = throw(error("Boom"))
-handle{verb}(r::Request{verb}) = Response("That was a $verb request")
+handle(r::Request{verb}) where verb = Response("That was a $verb request")
